2a6d307b5c1946ef9862c8a99a9ebff9e29c5e38
Lucas Mace <lucasmace4130@gmail.com>
2020-11-23 13:27:54 -0600

    Scope Issue. Using a global Outputs() for the moment

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+let max = 600; // Size of #canvas. Should be dynamic --
+let shapeList = [];
+let shapeCount = 0;
+
 class Shape {
     constructor() {
+        this.arrayPos = shapeCount;
+        this.xPos = this.randomVal(0, max);
+        this.yPos = this.randomVal(0, max);
         this.div = $('<div></div>');
-        this.div.addClass('shape');
-        this.xPos = randomVal(0, max);
-        this.yPos = randomVal(0, max);
         this.div.css({
             "background-color": this.ranColor(),
             "left": this.xPos,
             "top": this.yPos
+        });
+        this.div.attr('arraypos', this.arrayPos);
+
+        this.div.click(function () {
+            outputs($(this).attr('class'), $(this).css("width"), $(this).css("height"));
         });
     }
 
@@ -19,9 +28,20 @@ class Shape {
         return output;
     }
 
-    // this.div.click(function () {
-    //     outputs("Square", width, height);
-    // });
+    randomVal(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    outputs(type, width, height) {
+        console.log("Found Me!");
+    }
+}
+
+function outputs(type, width, height) {
+    console.log(width);
+    console.log(height);
+    if (type == "square" && width != height) { type = "rectangle"; }
+    console.log(type);
 }
 
 class Circle extends Shape {
@@ -37,8 +57,9 @@ class Circle extends Shape {
 }
 
 class Square extends Shape {
-    constructor(width, height = width){
+    constructor(width, height = width) {
         super();
+        this.div.addClass('square');
         this.div.css({
             "height": height,
             "width": width
@@ -47,28 +68,21 @@ class Square extends Shape {
     }
 }
 
-function outputs(type, width, hight) {
-    console.log(type, width, height);
-}
-
-let max = 600; // Size of #canvas. Should be dynamic --
-function randomVal(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
 $('#add-square').click(function () {
     let width = $('#add-square-input').val();
-    s1 = new Square(width);
+    shapeList[shapeCount] = new Square(width);
+    shapeCount++;
 });
 
 $('#add-rectangle').click(function () {
     let width = $('#add-rectangle-width-input').val();
     let height = $('#add-rectangle-height-input').val();
-    s1 = new Shape;
-    s1.square(width, height);
+    shapeList[shapeCount] = new Square(width, height);
+    shapeCount++;
 });
 
 $('#add-circle').click(function () {
     let radius = $('#add-circle-input').val();
-    s1 = new Circle(radius);
+    shapeList[shapeCount] = new Circle(radius);
+    shapeCount++;
 });
