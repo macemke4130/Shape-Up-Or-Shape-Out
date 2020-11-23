5f6a6451aa2dc1be7c23dc2017c6b91abaea67d7
Lucas Mace <lucasmace4130@gmail.com>
2020-11-23 16:47:46 -0600

    Working on final functionality

--- app.js
+++ app.js
@@ -3,34 +3,12 @@ let shapeList = [];
 let shapeCount = 0;
 
 class Shape {
-    constructor(type, width, height) {
-        this.type = type;
-        this.width = width;
-        this.height = height;
-        this.arrayPos = shapeCount;
-        this.xPos = this.randomVal(0, max);
-        this.yPos = this.randomVal(0, max);
+    constructor(width) {
         this.div = $('<div></div>');
-        this.div.css({
-            "background-color": this.ranColor(),
-            "left": this.xPos,
-            "top": this.yPos
-        });
-        this.div.attr('arraypos', this.arrayPos);
+        this.div.addClass('shape');
 
-        this.div.click(function () {
-            outputs($(this).attr('class'), $(this).css("width"), $(this).css("height"));
-        });
-
-        //this.div.click(() => this.outputs());
-    }
-
-    ranColor() {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        let output = `rgb(${r}, ${g}, ${b})`;
-        return output;
+        this.div.click(() => this.outputs(this.type, this.width, this.height));
+        this.div.dblclick(() => this.removeShape());
     }
 
     randomVal(min, max) {
@@ -38,59 +16,65 @@ class Shape {
     }
 
     outputs(type, width, height) {
-        console.log("Found Me!");
+        this.clearOutputs();
+        if (type == "square" && width != height) { type = "rectangle"; }
+        
+        type = type.charAt(0).toUpperCase() + type.slice(1); // Capitalizes first character --
+
+        $('#shape-output').val(type);
+        $('#width-output').val(width + "px");
+        $('#height-output').val(height + "px");
+
+        switch (type) {
+            case "Circle":
+                $('#radius-output').val(width / 2 + "px");
+                $('#area-output').val(Math.floor(Math.PI * ((width / 2) * (width / 2))) + "px");
+                $('#perimeter-output').val(Math.floor(2 * Math.PI * (width / 2)) + "px");
+                break;
+
+            case "Triangle":
+                $('#area-output').val((width * height) / 2);
+                $('#radius-output').val("N/A");
+                $('#perimeter-output').val((Math.floor(Math.sqrt((width * width) + (width * width)) + (width * 2))) + "px");
+                break;
+
+            default:
+                $('#area-output').val(width * height + "px");
+                $('#perimeter-output').val(width * 2 + height * 2 + "px");
+                $('#radius-output').val("N/A");
+                break;
+        }
     }
-}
 
-function outputs(type, width, height) {
-    clearOutputs();
-    if (type == "square" && width != height) { type = "rectangle"; }
-    type = type.charAt(0).toUpperCase() + type.slice(1); // Capitalizes first character --
-
-    $('#shape-output').val(type);
-    $('#width-output').val(width);
-    $('#height-output').val(height);
-
-    // Removes "px" from variables --
-    width = width.slice(0, width.length - 2);
-    height = height.slice(0, height.length - 2);
-
-    switch (type) {
-        case "Circle":
-            $('#radius-output').val(width / 2 + "px");
-            $('#area-output').val(Math.floor(Math.PI * ((width / 2) * (width / 2))) + "px");
-            $('#perimeter-output').val(Math.floor(2 * Math.PI * (width / 2)) + "px");
-            break;
-
-        case "Triangle":
-            $('#area-output').val((width * height) / 2);
-            $('#radius-output').val("N/A");
-            $('#perimeter-output').val((Math.floor(Math.sqrt((width * width) + (width * width)) + (width * 2))) + "px");
-            break;
-
-        default:
-            $('#area-output').val(width * height + "px");
-            $('#perimeter-output').val(width * 2 + height * 2 + "px");
-            $('#radius-output').val("N/A");
-            break;
+    clearOutputs() {
+        $('#shape-output').val("");
+        $('#width-output').val("");
+        $('#height-output').val("");
+        $('#radius-output').val("");
+        $('#area-output').val("");
+        $('#perimeter-output').val("");
     }
-}
 
-function clearOutputs(){
-    $('#shape-output').val("");
-    $('#width-output').val("");
-    $('#height-output').val("");
-    $('#area-output').val("");
-    $('#perimeter-output').val("");
-
+    removeShape(){
+       $(this.div).remove();
+       this.clearOutputs();
+    }
 }
 
 class Circle extends Shape {
     constructor(radius) {
         super();
         this.type = "circle";
+        this.width = radius * 2;
+        this.height = radius * 2;
+        this.xPos = this.randomVal(0, (max - this.width));
+        this.yPos = this.randomVal(0, (max - this.height));
+
         this.div.addClass('circle');
         this.div.css({
+            "left": this.xPos,
+            "top": this.yPos,
+            "background-color": "purple",
             "height": radius * 2,
             "width": radius * 2
         });
@@ -102,12 +86,18 @@ class Triangle extends Shape {
     constructor(width) {
         super();
         this.type = "triangle";
+        this.width = width;
+        this.height = width;
+        this.xPos = this.randomVal(0, (max - this.width));
+        this.yPos = this.randomVal(0, (max - this.height));
+
         this.div.addClass('triangle');
         this.div.css({
-            "background-color": "transparent", // Overrides Shape Class Decloration --
+            "left": this.xPos,
+            "top": this.yPos,
+            "background-color": "transparent",
             "border-width": width + "px 0 0 " + width + "px",
-            "border-color": "transparent transparent transparent " + this.ranColor(),
-            "transform": "rotate(135deg)"
+            "border-color": "transparent transparent transparent yellow"
         });
         $('#canvas').append(this.div);
     }
@@ -117,8 +107,21 @@ class Square extends Shape {
     constructor(width, height = width) {
         super();
         this.type = "square";
-        this.div.addClass('square');
+        this.width = width;
+        this.height = height;
+        this.xPos = this.randomVal(0, (max - this.width));
+        this.yPos = this.randomVal(0, (max - this.height));
+        let bgColor;
+        if(this.width == this.height){
+            bgColor = "red";
+        } else {
+            bgColor = "green";
+        }
+
         this.div.css({
+            "left": this.xPos,
+            "top": this.yPos,
+            "background-color": bgColor,
             "height": height,
             "width": width
         });
